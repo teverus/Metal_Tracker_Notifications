947c8207a8c3ba17c2d645042a329528f709076b
iserdyuk <teverus@yandex.ru>
2023-02-06 09:49:49 +0300

    [WIP] SyncBandsWithPCloudScreen.py

--- Code/Screens/SyncBandsWithPCloudScreen.py
+++ Code/Screens/SyncBandsWithPCloudScreen.py
@@ -1,21 +1,32 @@
+from time import sleep
+
 from Code.TeverusSDK.Screen import (
     Screen,
     Action,
-    do_nothing,
     SCREEN_WIDTH,
-    GO_BACK_ACTION,
+    show_message,
 )
-from Code.TeverusSDK.Table import Table
+from Code.TeverusSDK.Table import Table, WHITE
 
 
 class SyncBandsWithPCloudScreen(Screen):
     def __init__(self):
-        self.actions = [Action(function=do_nothing)]
+        self.actions = [
+            Action(function=self.print_hello_world, immediate_action=True, go_back=True)
+        ]
 
         self.table = Table(
             table_title="Sync tracked bands with pCloud",
             table_width=SCREEN_WIDTH,
-            footer=[GO_BACK_ACTION],
+            rows_bottom_border=False,
+            highlight=False,
         )
 
         super(SyncBandsWithPCloudScreen, self).__init__(self.table, self.actions)
+
+    @staticmethod
+    def print_hello_world():
+        print(" Syncing bands... ")
+        sleep(1)
+        print(" Syncing bands... Done")
+        show_message(("Bands synched with pCloud", WHITE))
