8cfd0e9ca300e1e00f4aa70142ba2b610f4c4930
iserdyuk <teverus@yandex.ru>
2023-02-06 07:51:12 +0300

    [Done] WelcomeScreen

---
+++ main.py
@@ -0,0 +1,40 @@
+from Code.TeverusSDK.Screen import (
+    Screen,
+    Action,
+    SCREEN_WIDTH,
+    QUIT_ACTION,
+    do_nothing,
+)
+from Code.TeverusSDK.Table import Table
+
+
+class WelcomeScreen(Screen):
+    def __init__(self):
+        self.actions = [
+            Action(
+                name="Check new albums on metal-tracker.com",
+                function=do_nothing,
+            ),
+            Action(
+                name="Synch tracked albums with pCloud",
+                function=do_nothing,
+            ),
+            Action(
+                name="Add/remove tracked albums manually",
+                function=do_nothing,
+            ),
+        ]
+
+        self.table = Table(
+            table_title="Metal-tracker.com notifications",
+            rows=[action.name for action in self.actions],
+            rows_bottom_border="-",
+            table_width=SCREEN_WIDTH,
+            footer=[QUIT_ACTION],
+        )
+
+        super(WelcomeScreen, self).__init__(self.table, self.actions)
+
+
+if __name__ == "__main__":
+    WelcomeScreen()
