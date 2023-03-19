47df6d056f8b70e6a3179a185025b7128cbd1293
iserdyuk <teverus@yandex.ru>
2023-03-19 07:36:06 +0300

    [WIP] New load bar

---
+++ Code/Modules/CheckNewAlbums.py
@@ -0,0 +1,163 @@
+import re
+from datetime import datetime
+from time import sleep
+
+from pandas import DataFrame
+from playwright.sync_api import sync_playwright
+
+from Code.AlbumEntry import AlbumEntry
+from Code.TeverusSDK.Screen import show_message, SCREEN_WIDTH
+from Code.TeverusSDK.Table import WHITE, Table
+
+HOST = "https://www.metal-tracker.com"
+METAL_TRACKER_SEARCH_URL = f"{HOST}/torrents/search.html"
+URL = "URL"
+BAND_NAME = "Name"
+COLUMNS = [BAND_NAME, "Album", "Year", URL, "Status"]
+KNOWN = "known"
+NEW = "new"
+
+
+class CheckNewAlbums:
+    def __init__(self, main):
+        # --- VARIABLES ----------------------------------------------------------------
+        self.time_start = datetime.now()
+        self.page = None
+        self.valid_albums = None
+        self.found_albums = None
+        self.main = main
+        self.database = self.main.database
+        self.max_bands = self.main.max_bands
+        self.bands_number = self.main.bands_number
+        self.max_length = self.main.max_length
+
+        # --- MAIN ACTION --------------------------------------------------------------
+        with sync_playwright() as self.p:
+            self.open_metal_tracker()
+
+            for index, band in enumerate(self.main.bands, 1):
+                self.index = index
+                self.band = band
+
+                self.search_for_albums()
+                self.check_found_albums()
+                self.show_info()
+
+            show_message("All albums were processed", WHITE)
+
+    ####################################################################################
+    #    HELPERS                                                                       #
+    ####################################################################################
+
+    def open_metal_tracker(self):
+        print(" Opening metal-tracker.com...")
+        self.page = self.get_page()
+        for _ in range(3):
+            try:
+                self.page.goto(METAL_TRACKER_SEARCH_URL, wait_until="domcontentloaded")
+                break
+            except Exception:
+                print(" Trying once again...")
+                sleep(1)
+        else:
+            raise Exception(f"\n[ERROR] Couldn't open {METAL_TRACKER_SEARCH_URL}")
+
+        print(" Opening metal-tracker.com... Done")
+
+    def get_page(self, headless=True):
+        browser = self.p.firefox.launch(headless=headless)
+        context = browser.new_context(viewport={"width": 1920, "height": 1080})
+        page = context.new_page()
+
+        return page
+
+    def search_for_albums(self):
+        search_input = "//input[@id='SearchTorrentsForm_nameTorrent']"
+        country = "//select[@id='SearchTorrentsForm_country']"
+        finland = "4"
+        search_button = "//input[@id='submitForm']"
+
+        self.page.locator(search_input).clear()
+        self.page.locator(search_input).type(self.band)
+        self.page.locator(country).select_option(finland)
+        self.page.locator(search_button).click()
+        self.page.wait_for_load_state("domcontentloaded")
+        sleep(2)
+
+        self.found_albums = self.page.locator("//div[@class='smallalbum']").all()
+
+    def check_found_albums(self):
+        self.valid_albums = {KNOWN: 0, NEW: 0}
+
+        for album in self.found_albums:
+            info = album.inner_text()
+
+            artist_and_album_title = info.split("\n")[0]
+            entry = AlbumEntry()
+
+            # --- Band -----------------------------------------------------------------
+            entry.band = self.get_artist_name(artist_and_album_title)
+
+            if entry.band == self.band:
+                # --- Album ------------------------------------------------------------
+                entry.title = re.findall(r" - (.*)", artist_and_album_title)[0]
+
+                # --- Year -------------------------------------------------------------
+                YEAR = "Год: "
+                result = info.split(YEAR)[-1].split("\n")[0] if YEAR in info else "----"
+                entry.year = result
+
+                # --- Url --------------------------------------------------------------
+                unique_part = album.locator("//a").first.get_attribute("href")
+                entry.url = f"{HOST}{unique_part}"
+
+                # --- Check if album exists in DB --------------------------------------
+                album_exists = self.database.check_if_value_exists(entry.url, "URL")
+
+                # --- Add to DB if needed ----------------------------------------------
+                if not album_exists:
+                    df = DataFrame([], columns=COLUMNS)
+                    df.loc[0] = [entry.band, entry.title, entry.year, entry.url, "New"]
+                    self.database.append_to_table(df, sort_by=BAND_NAME)
+
+                # --- Add to self.valid_albums for indication --------------------------
+                target_key = KNOWN if album_exists else NEW
+                self.valid_albums[target_key] += 1
+
+    def get_artist_name(self, artist_and_album_title):
+        name = artist_and_album_title.split(" - ")[0]
+        name = name.replace("...", "") if "..." in name else name
+
+        delimiters = [char for char in ["/", ",", "&"] if char in name]
+        if delimiters:
+            assert len(delimiters) == 1, f"{delimiters = }"
+            artist_name = self.split_and_check(name, delimiters[0])
+        else:
+            artist_name = name
+
+        return artist_name
+
+    def split_and_check(self, album_artist_name, character):
+        artists = album_artist_name.split(character)
+        for artist in artists:
+            artist = artist.strip()
+            if artist == self.band:
+                return artist
+
+        return []
+
+    def show_info(self):
+        time_elapsed = str(datetime.now() - self.time_start).split(".")[0]
+
+        percent_done = int(self.index / self.main.bands_number * 100)
+        percent_free = 100 - percent_done
+        taken = "#" * percent_done
+        empty = "." * percent_free
+        load_bar = f"{time_elapsed} [{taken}{empty}] {percent_done:2} %"
+
+        Table(
+            table_title="Checking new albums on metal-tracker.com",
+            table_width=SCREEN_WIDTH,
+            rows=load_bar,
+            rows_bottom_border=False,
+        ).print_table()
--- Code/Screens/CheckNewAlbumsScreen.py
+++ Code/Screens/CheckNewAlbumsScreen.py
@@ -1,24 +1,10 @@
-import re
-from datetime import datetime
 from pathlib import Path
-from time import sleep
 
-from pandas import DataFrame
-from playwright.sync_api import sync_playwright
-
-from Code.AlbumEntry import AlbumEntry
+from Code.Modules.CheckNewAlbums import CheckNewAlbums
 from Code.TeverusSDK.DataBase import DataBase
-from Code.TeverusSDK.Screen import Screen, Action, SCREEN_WIDTH, show_message
-from Code.TeverusSDK.Table import Table, WHITE
+from Code.TeverusSDK.Screen import Screen, Action, SCREEN_WIDTH
+from Code.TeverusSDK.Table import Table
 from Code.TeverusSDK.YamlTool import YamlTool
-
-HOST = "https://www.metal-tracker.com"
-METAL_TRACKER_SEARCH_URL = f"{HOST}/torrents/search.html"
-URL = "URL"
-BAND_NAME = "Name"
-COLUMNS = [BAND_NAME, "Album", "Year", URL, "Status"]
-KNOWN = "known"
-NEW = "new"
 
 
 class CheckNewAlbumsScreen(Screen):
@@ -29,15 +15,11 @@ class CheckNewAlbumsScreen(Screen):
         self.bands_number = len(self.bands)
         self.max_bands = len(str(self.bands_number))
         self.max_length = max([len(b) for b in self.bands])
-        self.found_albums = None
-        self.page = None
-        self.band = None
-        self.index = None
-        self.valid_albums = None
 
         self.actions = [
             Action(
-                function=self.check_new_albums,
+                function=CheckNewAlbums,
+                arguments={"main": self},
                 immediate_action=True,
                 go_back=True,
             )
@@ -51,121 +33,3 @@ class CheckNewAlbumsScreen(Screen):
         )
 
         super(CheckNewAlbumsScreen, self).__init__(self.table, self.actions)
-
-    def check_new_albums(self):
-        with sync_playwright() as self.p:
-            time_start = datetime.now()
-            print(" Opening metal-tracker.com...")
-            self.page = self.get_page()
-            for _ in range(3):
-                try:
-                    self.page.goto(METAL_TRACKER_SEARCH_URL)
-                    break
-                except Exception:
-                    print(" Trying once again...")
-                    sleep(1)
-            else:
-                raise Exception(f"\n[ERROR] Couldn't open {METAL_TRACKER_SEARCH_URL}")
-
-            print(" Opening metal-tracker.com... Done")
-
-            for index, band in enumerate(self.bands, 1):
-                self.index = index
-                self.band = band
-
-                self.search_for_albums()
-                self.check_found_albums()
-                self.show_info()
-
-            time_finish = datetime.now()
-            time_diff = str(time_finish - time_start).split(".")[0]
-            show_message(f"All albums were processed in {time_diff}", WHITE)
-
-    def get_page(self, headless=True):
-        browser = self.p.firefox.launch(headless=headless)
-        context = browser.new_context(viewport={"width": 1920, "height": 1080})
-        page = context.new_page()
-
-        return page
-
-    def search_for_albums(self):
-        search_input = "//input[@id='SearchTorrentsForm_nameTorrent']"
-        country = "//select[@id='SearchTorrentsForm_country']"
-        finland = "4"
-        search_button = "//input[@id='submitForm']"
-
-        self.page.locator(search_input).clear()
-        self.page.locator(search_input).type(self.band)
-        self.page.locator(country).select_option(finland)
-        self.page.locator(search_button).click()
-        self.page.wait_for_load_state("networkidle")
-        sleep(2)
-
-        self.found_albums = self.page.locator("//div[@class='smallalbum']").all()
-
-    def check_found_albums(self):
-        self.valid_albums = {KNOWN: 0, NEW: 0}
-
-        for album in self.found_albums:
-            info = album.inner_text()
-            artist_and_album_title = info.split("\n")[0]
-            entry = AlbumEntry()
-
-            # --- Band -----------------------------------------------------------------
-            entry.band = self.get_artist_name(artist_and_album_title)
-
-            if entry.band == self.band:
-                # --- Album ------------------------------------------------------------
-                entry.title = re.findall(r" - (.*)", artist_and_album_title)[0]
-
-                # --- Year -------------------------------------------------------------
-                YEAR = "Год: "
-                result = info.split(YEAR)[-1].split("\n")[0] if YEAR in info else "----"
-                entry.year = result
-
-                # --- Url --------------------------------------------------------------
-                unique_part = album.locator("//a").first.get_attribute("href")
-                entry.url = f"{HOST}{unique_part}"
-
-                # --- Check if album exists in DB --------------------------------------
-                album_exists = self.database.check_if_value_exists(entry.url, "URL")
-
-                # --- Add to DB if needed ----------------------------------------------
-                if not album_exists:
-                    df = DataFrame([], columns=COLUMNS)
-                    df.loc[0] = [entry.band, entry.title, entry.year, entry.url, "New"]
-                    self.database.append_to_table(df, sort_by=BAND_NAME)
-
-                # --- Add to self.valid_albums for indication --------------------------
-                target_key = KNOWN if album_exists else NEW
-                self.valid_albums[target_key] += 1
-
-    def get_artist_name(self, artist_and_album_title):
-        name = artist_and_album_title.split(" - ")[0]
-        name = name.replace("...", "") if "..." in name else name
-
-        delimiters = [char for char in ["/", ",", "&"] if char in name]
-        if delimiters:
-            assert len(delimiters) == 1, f"{delimiters = }"
-            artist_name = self.split_and_check(name, delimiters[0])
-        else:
-            artist_name = name
-
-        return artist_name
-
-    def split_and_check(self, album_artist_name, character):
-        artists = album_artist_name.split(character)
-        for artist in artists:
-            artist = artist.strip()
-            if artist == self.band:
-                return artist
-
-        return []
-
-    def show_info(self):
-        percent = str(int(self.index / self.bands_number * 100)).rjust(3)
-        info = f"{str(self.index).rjust(self.max_bands)}/{self.bands_number}|{percent}%"
-        band_adjusted = str(self.band).ljust(self.max_length)
-        known_albums = "#" * self.valid_albums[KNOWN]
-        new_albums = "!" * self.valid_albums[NEW]
-        print(f" [{info}] {band_adjusted} {new_albums}{known_albums}")
